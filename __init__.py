283527d88845b29d664972ebc112951f595f5404
fundamovies <64239201+fundamovies@users.noreply.github.com>
2020-04-25 23:02:13 +0530

    Update __init__.py

---
+++ __init__.py
@@ -0,0 +1,2 @@
+#!/usr/bin/env python# -*- coding: utf-8 -*-# Copyright (C) 2019 Atul-Kadian class Creds(object): TOKEN = "1182230394:AAH0scw6Mj42PzOmSGcWASufgD0WrNa-too" CLIENT_ID = "987971473847-bk8hd9rj0os04koevu239k74i7jrkvn8.apps.googleusercontent.com" CLIENT_SECRET = "D37StFL53H6L6vXjsAKsYQJD"
+
